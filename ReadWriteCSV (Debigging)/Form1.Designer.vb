6e717a4effb8c5c9c73882302c8432d99b482cdd
giorgia.darmiento <giorgia.darmiento@PCGIORGIA>
2022-10-10 22:19:21 +0200

    Aggiungere i file di progetto.

---
+++ ReadWriteCSV (Debigging)/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(159, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Read"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(45, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "GetFile"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(45, 42)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(702, 54)
+        Me.RichTextBox1.TabIndex = 2
+        Me.RichTextBox1.Text = ""
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.Location = New System.Drawing.Point(45, 120)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(702, 318)
+        Me.RichTextBox2.TabIndex = 3
+        Me.RichTextBox2.Text = ""
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents RichTextBox2 As RichTextBox
+End Class
---
+++ ReadWriteCSV (Debigging)/Form1.vb
@@ -0,0 +1,77 @@
+﻿Imports System.IO
+Imports Microsoft.VisualBasic.FileIO
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.RichTextBox1.EnableAutoDragDrop = True
+    End Sub
+
+    Private Sub RichTextBox1_DragDrop(ByVal sender As Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles RichTextBox1.DragDrop
+        e.Effect = DragDropEffects.None
+
+        Me.RichTextBox1.Clear()
+
+        Dim files() As String = CType(e.Data.GetData(DataFormats.FileDrop), String())
+
+        For Each path In files
+            Me.ProcessaFile(path)
+        Next
+    End Sub
+
+    Public Sub RichTextBox1_DragEnter(ByVal sender As Object, ByVal e As System.Windows.Forms.DragEventArgs) Handles RichTextBox1.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+    Sub ProcessaFile(path As String)
+        Me.RichTextBox1.AppendText(path & Environment.NewLine)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Path As String = "C:\Users\giorgia.darmiento\source\repos\ReadWriteCSV (Debigging)\population_csv.csv"
+
+        Dim ListOfUnits As New List(Of Country)
+
+
+        Using R As New TextFieldParser(Path)
+
+            R.Delimiters = New String() {","}
+
+            Dim NamesOfVariables As String = R.ReadLine
+
+            Do While Not R.EndOfData
+
+                Dim Values() As String = R.ReadFields()
+
+                Dim Country As New Country
+
+                'fille the fields with actual data
+
+                Country.CountryName = Values(0)
+                Country.CountryCode = Values(1)
+                Country.Year = Values(2)
+                Country.Value = Values(3)
+
+                ListOfUnits.Add(Country)
+            Loop
+
+        End Using
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim o As New OpenFileDialog
+        o.ShowDialog()
+
+        If String.IsNullOrWhiteSpace(o.FileName) Then Exit Sub
+        Me.RichTextBox1.Text = o.FileName
+
+    End Sub
+End Class
+
+Public Class Country
+    Public CountryName As String
+    Public CountryCode As String
+    Public Year As String
+    Public Value As String
+End Class
